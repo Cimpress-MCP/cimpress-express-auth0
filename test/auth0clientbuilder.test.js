f11d4a220424548a205d6e6da98feed948f51f22
Dave Purrington <dave.purrington@gmail.com>
2017-04-14 09:31:45 -0400

    More cleanup

--- test/auth0clientbuilder.test.js
+++ test/auth0clientbuilder.test.js
@@ -1,37 +1,37 @@
-// /*jshint -W030 */
-// var expect = require('chai').expect,
-//   MiddlewareHelper = require('./middlewaretesthelper.js'),
-//   JwtMock = require('./jwtmock.js'),
-//   chai = require('chai'),
-//   spies = require('chai-spies');
+/*jshint -W030 */
+var expect = require('chai').expect,
+  MiddlewareHelper = require('./middlewaretesthelper.js'),
+  JwtMock = require('./jwtmock.js'),
+  chai = require('chai'),
+  spies = require('chai-spies');
 
-// chai.use(spies);
+chai.use(spies);
 
-// describe('Verify the construction of an auth0 client.', function () {
+describe('Verify the construction of an auth0 client.', function () {
 
-//   var helper;
+  var helper;
 
-//   beforeEach(function () {
-//     helper = new MiddlewareHelper('../lib/auth0clientbuilder.js');
-//     helper.config = {
-//       app: {
-//         auth0: {
-//           domain: "this.isa.domain"
-//         }
-//       }
-//     };
+  beforeEach(function () {
+    helper = new MiddlewareHelper('../lib/auth0clientbuilder.js');
+    helper.config = {
+      app: {
+        auth0: {
+          domain: "this.isa.domain"
+        }
+      }
+    };
 
-//     // build custom endpoint returns a function representing the endpoint
-//     helper.res = { locals: { endpointClient: {} } };
-//     helper.res.locals.endpointClient.endpoint = chai.spy(function (obj) {
-//       return function () { };
-//     });
+    // build custom endpoint returns a function representing the endpoint
+    helper.res = { locals: { endpointClient: {} } };
+    helper.res.locals.endpointClient.endpoint = chai.spy(function (obj) {
+      return function () { };
+    });
 
-//   });
+  });
 
-//   it('Should construct an auth0 client', function () {
-//     helper.execute();
-//     expect(helper.req.getAuth0Client()).to.be.defined;
-//     expect(helper.res.locals.endpointClient.endpoint).to.have.been.called.with({ baseUrl: "https://this.isa.domain" });
-//   });
-// });
+  it('Should construct an auth0 client', function () {
+    helper.execute();
+    expect(helper.req.getAuth0Client()).to.be.defined;
+    expect(helper.res.locals.endpointClient.endpoint).to.have.been.called.with({ baseUrl: "https://this.isa.domain" });
+  });
+});
