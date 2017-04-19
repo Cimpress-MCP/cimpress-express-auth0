67e09074352b5e75de42170356cff52ba8430e7f
Dave Purrington <dpurrington@cimpress.com>
2017-04-19 13:49:30 -0400

    fix(package): Fix main (entry point) so that lib functions are exposed
    Add index.js file in lib to export all modules in lib.

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const should = chai.should();
+const lib = require('../lib');
+
+describe('Require index.js', function () {
+  it('Defines all exported functions', () => {
+    should.not.equal(null, lib.unauthorized);
+  })
+});
